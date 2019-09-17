8dc5cfe1a80175e43342cb3be80a0d38b611182f
nescirem <nescirem@gmail.com>
2019-09-17 22:00:49 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
+#!/bin/bash
+
 hexo generate
 python3 yasuo.py
 git add .
-git commit -m "change the committer"
+git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git push -u origin source:source
 cp -R public/* .deploy/nescirem.github.io
 cd .deploy/nescirem.github.io
 git add .
-git commit -m "change the committer"
+git commit -m "$(date "+%Y-%m-%d %H:%M:%S")"
 git push origin master
 cd ~/blog-git/blog
 cp -R public/* .deploy/nescirem
 cd .deploy/nescirem
 git add .
-git commit -m "Backuped 2019-02-27"
+git commit -m "Backuped $(date "+%Y-%m-%d %H:%M:%S")"
 git push origin master
